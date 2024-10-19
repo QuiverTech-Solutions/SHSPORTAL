5a78d0b0330d0812233387de9d379deb0551d41c
aee4 <eeyramagbetor@gmail.com>
2024-10-19 03:37:09 +0000

    update_at added to school model and repo

--- src/models/schools.py
+++ src/models/schools.py
@@ -1,7 +1,7 @@
 """Schools model."""
 
 from typing import Optional
-
+from datetime import datetime
 from pydantic import BaseModel, Field
 
 from src.models.base import DateTimeModelMixin, IDModelMixin_, IsDeletedModelMixin
@@ -13,11 +13,12 @@ class SchoolBase(BaseModel):
     name: str = Field(..., min_length=2, max_length=100)
     location: str = Field(..., min_length=2, max_length=100)
     registration_fee: float = Field(..., gt=0)
+    updated_at: Optional[datetime]  # This field can be auto-updated on each update operation
 
 
 class SchoolCreate(SchoolBase):
     """School create model"""
-
+    
     pass
 
 
@@ -27,10 +28,10 @@ class SchoolUpdate(BaseModel):
     name: Optional[str] = Field(None, min_length=2, max_length=100)
     location: Optional[str] = Field(None, min_length=2, max_length=100)
     registration_fee: Optional[float] = Field(None, gt=0)
+    updated_at: datetime = Field(default_factory=datetime.now)  # Set updated_at during updates
 
     class Config:
         """Configurations for the class."""
-
         validate_assignment = True
         arbitrary_types_allowed = True
         extra = "forbid"
@@ -38,11 +39,11 @@ class SchoolUpdate(BaseModel):
 
 class SchoolInDb(SchoolCreate, IsDeletedModelMixin, IDModelMixin_, DateTimeModelMixin):
     """School in DB model"""
-
-    pass
+    
+    updated_at: datetime  # Ensure this is stored in the database
 
 
 class SchoolPublic(SchoolBase, DateTimeModelMixin, IDModelMixin_):
     """School public model"""
 
-    pass
+    updated_at: datetime  # Expose this field in the public response
